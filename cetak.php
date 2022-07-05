4578cd02eb98302fbcabefd06899f79565e40052
Dheni Subenk <dsubenk@192.168.1.4>
2022-07-05 15:37:14 +0900

    dasar php

---
+++ cetak.php
@@ -0,0 +1,9 @@
+<?php
+    //var_dump($_POST);
+
+    $nama = $_GET['nama'];
+    $nilai = $_GET['nilai'];
+
+    echo "Halo $nama, Nilai Anda $nilai";
+
+?>
---
+++ week9.php
@@ -0,0 +1,27 @@
+<?php
+    //script php
+    //echo "Hello World";
+
+    //variabel
+    //$namavariabel = value;
+    $nama = "Kamarudin ";
+    $nama .= "Jaenudin";
+    //$Nama = "Udin";
+    $nilai = 90;
+    $mk = "Pengembangan Web";
+    $jurusan;
+
+    $x = 10;        //10
+    $x += 5;        // x = x + 5  ? x = 10 + 5 //15
+    $x += 15;       // x = x + 15 ? x = 15 + 15 //30
+
+    echo "Nama : ".$nama;
+
+    echo "<br>Haloo $nama <br>";
+    echo 'Haloo '.$nama.' <br>';
+
+    echo "<h1 align=\"center\">$x</h1>";
+    echo '<h1 align="center">$nama</h1>';
+
+    var_dump($nama);
+?>
